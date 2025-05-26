998fd98f39f0bddb382111cdae2ac62ebd89183b
austine <46765685+austine299@users.noreply.github.com>
2025-05-26 04:41:51 -0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use("/api/auth", authRoutes);
+app.use(authRoutes);
 
 // ✅ Start the server
 const PORT = process.env.PORT || 5000;
