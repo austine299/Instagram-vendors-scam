c68f4d81368c9567750b62456ba08a1bd303b614
austine <46765685+austine299@users.noreply.github.com>
2025-05-31 04:29:49 -0700

    Update App.js

--- vendor/src/App.js
+++ vendor/src/App.js
@@ -42,7 +42,7 @@ function App() {
       }
 
       try {
-        const res = await axios.get("http://localhost:5000/myAccount", {
+        const res = await axios.get("https://instagram-vendors-vendor.onrender.com/myAccount", {
           headers: { Authorization: `Bearer ${token}` },
         });
         setUser(res.data);
